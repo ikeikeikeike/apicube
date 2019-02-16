3661e1fff622e5cf849fe7182d26217d98eb06a6
ikeikeikeike <jp.ne.co.jp@gmail.com>
2019-02-17 08:12:20 +0900

    added mock test

---
+++ rpc/handler/lifecycle/handler_test.go
@@ -0,0 +1,37 @@
+package lifecycle
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/ikeikeikeike/apicube/rpc/mockprotocol/mock_lifecycle"
+	pb "github.com/ikeikeikeike/apicube/rpc/protocol/apicube/lifecycle"
+)
+
+func TestPing(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := mock_lifecycle.NewMockPingServiceClient(ctrl)
+
+	client.EXPECT().GetPing(
+		gomock.Any(),
+		&pb.Ping{Msg: "one-two-token"},
+	).Return(&pb.Ping{
+		Msg: "one-two-token",
+	}, nil)
+
+	t.Helper()
+
+	resp, err := client.GetPing(
+		context.Background(),
+		&pb.Ping{Msg: "one-two-token"},
+	)
+	if err != nil && resp.Msg != "one-two-token" {
+		t.Errorf("mocking failed")
+	}
+
+	t.Log("Reply : ", resp)
+}
